74dc4185a25c80fd71255958efee6bdd81f9981b
Juan Manuel Giulietti <juanmanuelgiulietti2@gmail.com>
2025-07-07 13:11:47 -0300

    Correcion de pruebas unitarias en slots

--- test_slots.py
+++ test_slots.py
@@ -1,10 +1,19 @@
 from slots import determinarResultado
 
 def test_gana_slots():
-    saldo, resultado = determinarResultado(["🍒", "🍒", "🍒"], 100, 500)
-    assert saldo > 500
+    pantalla = [
+        ["🍒", "🍒", "🍒"],
+        ["💎", "7️⃣", "🍋"],
+        ["🔔", "🍋", "💎"]
+    ]
+    saldo, resultado = determinarResultado(pantalla, 1000, 500)
+    assert saldo > 1000
 
 def test_pierde_slots():
-    saldo, resultado = determinarResultado(["🍒", "💎", "7️⃣"], 100, 500)
-    assert saldo == 500
-    
+    pantalla = [
+        ["🍒", "💎", "7️⃣"],
+        ["💎", "🍋", "🔔"],
+        ["7️⃣", "🍋", "💎"]
+    ]
+    saldo, resultado = determinarResultado(pantalla, 1000, 500)
+    assert saldo == 1000
